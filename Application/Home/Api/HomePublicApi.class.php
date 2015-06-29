020d7b32ba77d465117ad2f8d76ac688a1834ba5
hebidu <hebidu@Gooraye-006>
2015-06-29 17:05:30 +0800

    首页重做啊

---
+++ Application/Home/Api/HomePublicApi.class.php
@@ -0,0 +1,38 @@
+<?php
+// .-----------------------------------------------------------------------------------
+// | WE TRY THE BEST WAY 杭州博也网络科技有限公司
+// |-----------------------------------------------------------------------------------
+// | Author: 贝青<99701759@qq.com>
+// | Copyright (c) 2013-2016, http://www.itboye.com. All Rights Reserved.
+// |-----------------------------------------------------------------------------------
+namespace Home\Api;
+
+class HomePublicApi{
+	
+	/*
+	 * 文章
+	 * */
+	 const Post_QueryAll='Admin/Post/query';
+	 const Post_Query='Admin/Post/queryNoPaging';
+	 const Post_SaveByID='Admin/Post/saveByID';
+	 const Post_Add='Admin/Post/add';
+	 
+	 /*
+	 * 文章分类
+	 * */
+	 const Datatree_QueryAll='Admin/Datatree/query';
+	 const Datatree_Query='Admin/Datatree/queryNoPaging';
+	 const Datatree_SaveByID='Admin/Datatree/saveByID';
+	 const Datatree_Add='Admin/Datatree/add';
+	
+	/*
+	 * 商品
+	 * */
+//  const Product_QueryAll='Home/TaskProduct/query';
+//	const Product_Query='Home/TaskProduct/queryNoPaging';
+//	const Product_SaveByID='Home/TaskProduct/saveByID';
+//	const Product_Add='Home/TaskProduct/add';
+//	const Product_Del='Home/TaskProduct/delete';
+	
+}
+
